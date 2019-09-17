4ea6d9aa232342be442f1c957eb3b032c0ef3650
satyam <39213202+krsatyam20@users.noreply.github.com>
2019-09-17 18:08:23 +0530

    Add files via upload

---
+++ Question.py
@@ -0,0 +1,54 @@
+'''#Question A3: WAP to find out the square of a given number.
+
+
+a=int(input("Please enter the number for square"))
+
+print(type(a)) # use for know value type 
+b= a*a
+print("squara of a=" ,b)
+
+
+# WAP to input a number and print its cube
+a= int(input("Please enter the value"))
+b=a*a*a
+
+print("cube of a= ",b)
+
+#WAP to input radius and calculate the area and circumference of a circle.
+
+r= int(input("Please input the radius to calculate the area & circuference"))
+radius= 3.14*r*r
+print("value of r=",radius)
+circumference=2*3.14*r
+print ("value of r=",circumference)
+'''
+'''
+#WAP to swap the values of two integer variables
+(a)	Using extra variable
+(b)	Without using extra variable
+'''
+a= int(input("please enter the value"))
+b= int(input("please enter the value"))
+print("Without swap")
+print("a=",a)
+print("b=",b)
+
+c= a
+a=b
+b=c
+
+print("With swap")
+print("a=",a)
+print("b=",b)
+
+
+
+
+
+
+
+
+
+
+
+
