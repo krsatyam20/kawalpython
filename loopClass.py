a73d2e405582a483fde07a1bce05543e76b405c6
satyam <39213202+krsatyam20@users.noreply.github.com>
2019-10-10 16:19:47 +0530

    Add files via upload

---
+++ loopClass.py
@@ -0,0 +1,57 @@
+#for x in range(
+
+'''
+1  -  100
+1  -----100 
+
+,,,
+
+
+    
+
+
+
+
+'''
+#for x in range(1,100):
+     #print(x)
+    #print("Hello kawal")
+    #if x%2==0:
+    #print("even number is= ",x)
+
+#Q1: 2*1=2
+#2*2=4
+
+for x in range (1,11):
+ a=x*2
+ print ("2 *",x,"=",a)
+
+
+#Q2: 1+2+3......10=? 
+
+a=1
+for x in range (1,11):
+    a=a+x
+print ("Value of x is =",a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
